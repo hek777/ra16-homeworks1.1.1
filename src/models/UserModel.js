47a89ce9f8e0afef7afb042f6b05bdee4cd782ef
Voinov Nikita <nikita-nikita2012@mail.ru>
2021-02-02 22:46:16 +0300

    Add all my files

---
+++ src/models/UserModel.js
@@ -0,0 +1,13 @@
+class UserModel {
+    constructor (id, brand, title, description, descriptionFull, price, currency  ) {
+        this.id = id;
+        this.brand = brand;
+        this.title = title;
+        this.description = description;
+        this.descriptionFull = descriptionFull;
+        this.price = price;
+        this.currency = currency;
+    }
+}
+
+
